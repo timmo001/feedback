dc65ee0cef5eb8c99b553e846e3af8947c3cfda1
Timmo <contact@timmo.xyz>
2020-04-01 16:47:59 +0100

    cleanup

--- api/test/services/feedback.test.ts
+++ api/test/services/feedback.test.ts
@@ -1,6 +1,6 @@
 import app from '../../src/app';
 
-describe('\'feedback\' service', () => {
+describe("'feedback' service", () => {
   it('registered the service', () => {
     const service = app.service('feedback');
     expect(service).toBeTruthy();
